5b7fdf0993055419b790cc27a13f0f10c4a3bee8
alcaras <alcaras@subcreation.net>
2013-02-17 12:05:59 -0500

    relative paths in init working now

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 from dateutil import parser
 
+import os
+
 import time
 from flask import g
 import string
@@ -16,15 +18,18 @@ app.jinja_env.globals['momentjs'] = momentjs
 from version import version
 app.jinja_env.globals['VERSION'] = version
 
+BASE_PATH = os.path.dirname(__file__)
+
+
 # need to pull in the date from the file and put it in a global variable
-f = open('/home/dominik/dota/app/version-date.txt', 'r')
+f = open(BASE_PATH + '/version-date.txt', 'r')
 data = f.read()
 version_date = parser.parse(data)
 f.close()
 app.jinja_env.globals['VERSION_DATE'] = version_date
 
 # pull in git describe
-f = open('/home/dominik/dota/app/version-describe.txt', 'r')
+f = open(BASE_PATH + '/version-describe.txt', 'r')
 data = f.read()
 version_describe = string.replace(string.replace(data, '\n', ''), '\r', '')
 f.close()
