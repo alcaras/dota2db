997e6c1ac0430f88692a15368baf131e96f16386
alcaras <alcaras@subcreation.net>
2013-09-17 02:16:49 -0400

    paths are fun

--- app/fpr.py
+++ app/fpr.py
@@ -1,5 +1,7 @@
 import cPickle as pickle
+import os
 
-jar = open('app/fpr_minmax.pickle', 'rb')
+basedir = os.path.abspath(os.path.dirname(__file__))
+jar = open(os.path.join(basedir, 'fpr_minmax.pickle'), 'rb')
 (fantasy_min, fantasy_max) = pickle.load(jar)
 jar.close()
